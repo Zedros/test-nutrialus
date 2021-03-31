48a6d3909e0484e81cd764e36dddfd0f21a37335
Sedros <sedros@users.noreply.github.com>
2021-03-31 15:57:45 -0300

    first

--- App.tsx
+++ App.tsx
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading'
+import { StatusBar } from 'expo-status-bar'
+import React, { useEffect, useState } from 'react'
+import { StyleSheet, Text, View, Button, Alert } from 'react-native'
+
+import UserData from './components/UserData'
+const urlDataUsuario = 'https://0q27loouph.execute-api.us-east-1.amazonaws.com/'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [dataUsuario, setDataUsuario] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+
+  // useEffect(() => {
+  //   (async () => {
+  //     await fetchDataUsuario(urlDataUsuario)
+  //   })();
+  // }, [])
+
+  async function fetchDataUsuario(URL: string) {
+    const respuesta = await fetch(URL)
+    const resultados = await respuesta.json()
+    setDataUsuario(resultados)
+    return resultados
+  }
+
+  if (!loading) {
+    return (
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <UserData dataUsuario={dataUsuario} />
+        <Button title="Actualizar" onPress={() => fetchDataUsuario(urlDataUsuario)} />
+      </View>
+    );
+  }
+  else {
+    return (
+      <AppLoading
+        startAsync={() => fetchDataUsuario(urlDataUsuario)}
+        onFinish={() => setLoading(false)}
+        onError={console.warn}
+      />
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'smoke',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ components/UserData.tsx
@@ -0,0 +1,97 @@
+import React from 'react'
+import { StyleSheet, Text, View, Image } from 'react-native'
+
+export default function UserData({ dataUsuario }: any) {
+
+    const { email, image, name, nutritionist, phone } = dataUsuario;
+
+    return (
+        <View style={[styles.userCard, styles.shadowBox]} >
+            <View style={styles.userCardTop}>
+                <Image
+                    style={[styles.userImage,styles.shadowBox]}
+                    source={{
+                        uri: image,
+                        cache: 'reload',
+                    }}
+                    key={Date.now()}
+                />
+                <Text style={styles.nombreUsuario}>{name}</Text>
+
+            </View>
+            <View style={styles.userInformation}>
+                <Text style={styles.textUserInformation}>{nutritionist}</Text>
+                <Text style={styles.textUserInformation}>{email}</Text>
+                <Text style={styles.textUserInformation}>{phone}</Text>
+            </View>
+
+        </View>
+
+    )
+}
+
+const styles = StyleSheet.create({
+    nombreUsuario:{
+        fontSize: 18,
+        fontWeight: '700',
+        fontFamily: 'Arial',
+        color:'#2B2350'
+    },
+    userCard: {
+        position: 'relative',
+        height: 350,
+        width: 250,
+        backgroundColor: 'white',
+        borderRadius: 30
+    },
+    userCardTop: {
+        position: 'relative',
+        height: '50%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderTopRightRadius:30,
+        borderTopLeftRadius:30,
+        backgroundColor:'#F4A261'
+    },
+    userInformation: {
+        display: 'flex',
+        height: '50%',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        textAlign: 'center',
+        backgroundColor: 'white',
+        borderBottomRightRadius:30,
+        borderBottomLeftRadius:30,
+        fontFamily:'Arial'
+    },
+    userImage: {
+        alignSelf: 'center',
+        width: 100,
+        height: 100,
+        borderRadius: 100,
+        marginBottom : 10,
+        border: 'solid',
+        borderWidth: 3,
+        borderColor: 'white',
+    },
+    shadowBox: {
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 5,
+        },
+        shadowOpacity: 0.36,
+        shadowRadius: 6.68,
+
+        elevation: 11,
+    },
+    textUserInformation:{
+        fontFamily:'Helvetica',
+        fontSize:15,
+        color:'#2B2350'
+
+    }
+})
